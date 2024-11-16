715644d8a5e603305efedb60cd4052242ae0fc8c
Abdul Kalam <107694562+Azadak12@users.noreply.github.com>
2024-11-16 23:10:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-
 document.getElementById('showPopupBtn').addEventListener('click', function () {
     document.getElementById('popup').style.display = 'flex';
 });
-
 
 document.getElementById('closePopupBtn').addEventListener('click', function () {
     document.getElementById('popup').style.display = 'none';
 });
 
-
 document.getElementById('cancelBtn').addEventListener('click', function () {
     document.getElementById('popup').style.display = 'none';
 });
-
 
 document.getElementById('okBtn').addEventListener('click', function () {
     document.getElementById('popup').style.display = 'none';
