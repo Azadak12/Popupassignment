062fb350e6b744796d0c97d9a38490b6769332c6
Abdul Kalam <107694562+Azadak12@users.noreply.github.com>
2024-11-18 10:15:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-document.getElementById('showPopupBtn').addEventListener('click', function () {
-    document.getElementById('popup').style.display = 'flex';
-});
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('showPopupBtn').addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'flex';
+    });
 
-document.getElementById('closePopupBtn').addEventListener('click', function () {
-    document.getElementById('popup').style.display = 'none';
-});
+    document.getElementById('closePopupBtn').addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'none';
+    });
 
-document.getElementById('cancelBtn').addEventListener('click', function () {
-    document.getElementById('popup').style.display = 'none';
-});
+    document.getElementById('cancelBtn').addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'none';
+    });
 
-document.getElementById('okBtn').addEventListener('click', function () {
-    document.getElementById('popup').style.display = 'none';
+    document.getElementById('okBtn').addEventListener('click', function () {
+        document.getElementById('popup').style.display = 'none';
+    });
 });
